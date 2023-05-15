d0a66aac38a130bd6c89736aa2d227888bc06b52
Nit8 <47635411+Nit8@users.noreply.github.com>
2023-05-15 14:28:06 +0530

    Builder Usuage

---
+++ PersonBuilder.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleWebScraper
+{
+    class PersonBuilder
+    {
+        private string _firstname;
+        private string _lastname;
+        private int _age;
+        private int _eyecolor;
+        private int _haircolor;
+
+        public PersonBuilder()
+        {
+            SetDefaults();
+        }
+        private void SetDefaults()
+        {
+            _firstname = string.Empty;
+            _lastname = "Jaiswal";
+            _age = 0;
+            _eyecolor = 153;
+            _haircolor = 153;
+        }
+
+        public PersonBuilder WithFirstName(string firstname)
+        {
+            _firstname=firstname;
+            return this;
+        }
+        public PersonBuilder WithLastName(string lastname)
+        {
+            _lastname = lastname;
+            return this;
+        }
+        public PersonBuilder WithAge(int age)
+        {
+            _age = age;
+            return this;
+        }
+        public PersonBuilder WithHairColor(int haircolor)
+        {
+            _haircolor = haircolor;
+            return this;
+        }
+        public PersonBuilder WithEyeColor(int eyecolor)
+        {
+            _eyecolor = eyecolor;
+            return this;
+        }
+
+        public Person Build()
+        {
+            Person person=new Person(_firstname,_lastname,_age,_eyecolor,_haircolor);
+            return person;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
-﻿namespace SimpleWebScraper
+﻿using System;
+using System.Net;
+namespace SimpleWebScraper
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Person person1 = new Person("Nitesh", "Jaiswal", 22, 153, 153);
+            Console.WriteLine(person1.FirstName + " " + person1.LastName);
+            Person person3= new PersonBuilder().WithAge(15).Build();
+            Console.WriteLine(person3.Age);
         }
     }
 }
